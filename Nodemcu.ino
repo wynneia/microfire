64458651df0e85b03a910c6722b64f0c43982807
Fela <60063027+wynneia@users.noreply.github.com>
2025-01-02 13:40:04 +0700

    Use async http library

--- Nodemcu.ino
+++ Nodemcu.ino
@@ -1,19 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
-#include <ESP8266HTTPClient.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncHTTPClient.h>
 
 const char* ssid = "lynxAdapt";
 const char* password = "janice69";
 
 const char* apiUrl = "http://mikro.a2s.ltd/api/sensor-data";
 const char* pumpStatusUrl = "http://mikro.a2s.ltd/api/pump/status";
-const char* pumpControlUrl = "http://mikro.a2s.ltd/api/pump/control";
-
 const String bearerToken = "your_bearer_token";
 
 #define ARDUINO_RX D2
 #define ARDUINO_TX D1
 
+AsyncHTTPClient asyncHttpClient;
 SoftwareSerial arduinoSerial(ARDUINO_RX, ARDUINO_TX);
 
 int flameValue = 0;
@@ -21,17 +21,12 @@ int smokeValue = 0;
 bool pumpStatus = false;
 
 unsigned long lastArduinoReadTime = 0;
-unsigned long lastWebPostTime = 0;
-unsigned long lastPumpCheckTime = 0;
-
-const unsigned long arduinoReadInterval = 1000; // 1 second
-const unsigned long webPostInterval = 3000; // 3 seconds
-const unsigned long pumpCheckInterval = 5000; // 5 seconds
+const unsigned long arduinoReadInterval = 1000;
 
 void setup() {
   Serial.begin(9600);
   arduinoSerial.begin(9600);
-  
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -52,16 +47,6 @@ void loop() {
       parseSensorData(data);
     }
   }
-
-  if (currentMillis - lastWebPostTime >= webPostInterval) {
-    lastWebPostTime = currentMillis;
-    sendSensorData(flameValue, smokeValue);
-  }
-
-  if (currentMillis - lastPumpCheckTime >= pumpCheckInterval) {
-    lastPumpCheckTime = currentMillis;
-    fetchPumpStatus();
-  }
 }
 
 void parseSensorData(String data) {
@@ -72,64 +57,57 @@ void parseSensorData(String data) {
     flameValue = data.substring(0, firstComma).toInt();
     smokeValue = data.substring(firstComma + 1, secondComma).toInt();
   }
+
+  sendSensorDataAsync(flameValue, smokeValue);
+  fetchPumpStatusAsync();
 }
 
-void sendSensorData(int flameValue, int smokeValue) {
+void sendSensorDataAsync(int flameValue, int smokeValue) {
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    WiFiClient client;
-
-    http.begin(client, apiUrl);
-    http.addHeader("Content-Type", "application/json");
-    http.addHeader("Authorization", "Bearer " + bearerToken);
-
     String payload = String("{\"flameValue\":") + flameValue + 
                      ",\"smokeValue\":" + smokeValue + "}";
 
-    int httpResponseCode = http.POST(payload);
+    asyncHttpClient.begin(apiUrl);
+    asyncHttpClient.addHeader("Content-Type", "application/json");
+    asyncHttpClient.addHeader("Authorization", "Bearer " + bearerToken);
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("Response: " + response);
-    } else {
-      Serial.println("Error in sending POST request: " + String(httpResponseCode));
-    }
+    asyncHttpClient.onRequestComplete([](AsyncHTTPClientResponse* response) {
+      if (response->statusCode() > 0) {
+        Serial.println("Sensor data POST response: " + response->responseText());
+      } else {
+        Serial.println("Error in POST request");
+      }
+    });
 
-    http.end();
+    asyncHttpClient.POST(payload);
   } else {
     Serial.println("Error: No WiFi connection");
   }
 }
 
-void fetchPumpStatus() {
+void fetchPumpStatusAsync() {
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    WiFiClient client;
+    asyncHttpClient.begin(pumpStatusUrl);
+    asyncHttpClient.addHeader("Authorization", "Bearer " + bearerToken);
 
-    http.begin(client, pumpStatusUrl);
-    http.addHeader("Authorization", "Bearer " + bearerToken);
+    asyncHttpClient.onRequestComplete([](AsyncHTTPClientResponse* response) {
+      if (response->statusCode() > 0) {
+        String responseText = response->responseText();
+        Serial.println("Pump status response: " + responseText);
 
-    int httpResponseCode = http.GET();
+        bool newPumpStatus = responseText.indexOf("\"pumpStatus\":true") != -1;
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("Pump status response: " + response);
-
-      bool newPumpStatus = false;
-      if (response.indexOf("\"pumpStatus\":true") != -1) {
-        newPumpStatus = true;
+        if (newPumpStatus != pumpStatus) {
+          pumpStatus = newPumpStatus;
+          arduinoSerial.println(pumpStatus ? "PUMP_ON" : "PUMP_OFF");
+          Serial.println("Sent to Arduino: " + String(pumpStatus ? "PUMP_ON" : "PUMP_OFF"));
+        }
+      } else {
+        Serial.println("Error in fetching pump status");
       }
+    });
 
-      if (newPumpStatus != pumpStatus) {
-        pumpStatus = newPumpStatus;
-        arduinoSerial.println(pumpStatus ? "PUMP_ON" : "PUMP_OFF");
-        Serial.println("Sent to Arduino: " + String(pumpStatus ? "PUMP_ON" : "PUMP_OFF"));
-      }
-    } else {
-      Serial.println("Error in fetching pump status: " + String(httpResponseCode));
-    }
-
-    http.end();
+    asyncHttpClient.GET();
   } else {
     Serial.println("Error: No WiFi connection");
   }
